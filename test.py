c7ab7d274c62ee254f4433e61af23de0f3baf937
matx17 <matx17@student.bth.se>
2019-11-05 21:05:20 +0100

    Division

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-        def test_mulint(self):
-            rv =  self.app.get('/mul?A=3&B=7')
-            self.assertMultiLineEqual('21', rv.data)
-        def test_mulfloat(self):
-            rv =  self.app.get('/mul?A=3.3&B=7.3')
-            self.assertMultiLineEqual('24.09', rv.data)
-        def test_mulfrac(self):
-            rv =  self.app.get('/mul?A=5/3&B=3/3')
+        def test_divfloat(self):
+            rv =  self.app.get('/div?A=3.3&B=7.3')
+            self.assertMultiLineEqual('0.452054794521', rv.data)
+        def test_divfrac(self):
+            rv =  self.app.get('/div?A=5/3&B=3/3')
             self.assertMultiLineEqual('1.66666666667', rv.data)
-        def test_mulneg(self):
-            rv =  self.app.get('/mul?A=4.3&B=-2.1')
-            self.assertMultiLineEqual('-9.03', rv.data)
+        def test_divneg(self):
+            rv =  self.app.get('/div?A=4.3&B=-2.1')
+            self.assertMultiLineEqual('-2.04761904762', rv.data)
+       
+        
 
-
+if __name__ == '__main__':
+    unittest.main()
