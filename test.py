f86a6736a90121fbbebe98ffb9264491794d62ea
matx17 <matx17@student.bth.se>
2019-11-06 06:05:25 +0100

    division

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+import main 
+import unittest
+
+class MyTestCase(unittest.TestCase):
+
+        def setUp(self):
+            main.app.testing = True
+            self.app = main.app.test_client()
+
         def test_divfloat(self):
             rv =  self.app.get('/div?A=3.3&B=7.3')
             self.assertMultiLineEqual('0.452054794521', rv.data)
@@ -7,8 +16,7 @@
         def test_divneg(self):
             rv =  self.app.get('/div?A=4.3&B=-2.1')
             self.assertMultiLineEqual('-2.04761904762', rv.data)
-       
+      
         
-
 if __name__ == '__main__':
     unittest.main()
